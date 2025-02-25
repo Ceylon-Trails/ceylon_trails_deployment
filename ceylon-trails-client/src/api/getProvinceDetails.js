ffb71bbb8a1f3eea787ab3ddf45e019d56c9205c
Dinuk_Tharindu <ddt94119@gmail.com>
2025-02-25 16:06:03 +0530

    Update getProvinceDetails.js

--- ceylon-trails-client/src/api/getProvinceDetails.js
+++ ceylon-trails-client/src/api/getProvinceDetails.js
@@ -1,1 +1,26 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getProvinceDetails =  createAsyncThunk(
+    "getProvinceDetails",async(_,thunkApi)=>{
+      
+        try {
+            const res = await fetch("/cards" && "/api/cards",{
+                method : "GET",
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                credentials : "include"
+            })
+            if(!res.ok){
+                const errorResponse = await res.json();
+                return thunkApi.rejectWithValue(errorResponse.message || "Error getting data")
+            }
+            const data = await res.json();
+            return data;
+
+        } catch (error) {
+            return thunkApi.rejectWithValue(error.message)
+        }
+    }
+)
 
